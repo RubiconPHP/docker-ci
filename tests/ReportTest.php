d5ab53925e41cf78af1a444d36f241b9f44e75b2
ronan-gloo <sonotone13@gmail.com>
2015-05-14 21:03:01 +0200

    add failing test

--- tests/ReportTest.php
+++ tests/ReportTest.php
@@ -10,5 +10,6 @@ class ReportTest extends \PHPUnit_Framework_TestCase
     {
         $reporter = new Report();
         $this->assertInternalType('string', (string) $reporter);
+        $this->assertSame('1', 1);
     }
 }
